a27d41de12152c3e67fe832e612f4d6e675d6b00
PiotrMazanik <2202552@uad.ac.uk>
2026-01-28 13:34:55 +0000

    init commit

---
+++ Code/cloud.gd
@@ -0,0 +1,7 @@
+extends ParallaxLayer
+
+@export var CLOUD_SPEED = -15 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.motion_offset.x += CLOUD_SPEED * delta
---
+++ Code/player_controller.cs
@@ -0,0 +1,159 @@
+using Godot;
+
+public partial class player_controller : CharacterBody2D
+{
+    [Export] public TileMapLayer Tilemap;
+
+    private Camera2D _camera;
+    private AnimatedSprite2D _animationPlayer;
+    private GpuParticles2D _particle;
+    private PackedScene _spray;
+
+
+    private const float WALK_SPEED = 60.0f;
+    private const float MAX_SPEED = 350.0f;
+    private const float TIME_TO_MAX_SPEED = 1.0f;
+    private const float FRICTION = 600.0f;
+    private const float JUMP_VELOCITY = -400.0f;
+
+    private const float ACCELERATION = MAX_SPEED / TIME_TO_MAX_SPEED;
+
+    private bool spraying = false;
+
+    private float _gravity;
+    private bool _breaking = false;
+
+    public override void _Ready()
+    {
+        _camera = GetNode<Camera2D>("Camera2D");
+        _animationPlayer = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        _particle = GetNode<GpuParticles2D>("GPUParticles2D");
+
+        _gravity = (float)ProjectSettings.GetSetting("physics/2d/default_gravity");
+
+        _spray = ResourceLoader.Load<PackedScene>("res://Scenes/spray.tscn");
+
+        SetupCameraLimits();
+    }
+
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("spray"))
+        {
+            spraying = true;
+            _animationPlayer.Play("Paint");
+            var sprayInstance = _spray.Instantiate() as Node2D;
+            //sprayInstance.Material = (Material)sprayInstance.Material.Duplicate();
+            sprayInstance.Position = Position + new Vector2(_animationPlayer.FlipH ? -67 : 67, -32);
+            GetParent().AddChild(sprayInstance);
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        float dt = (float)delta;
+
+        // Gravity
+        if (!IsOnFloor())
+        {
+            Velocity += Vector2.Down * _gravity * dt;
+        }
+
+        // Jump
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
+        {
+            Velocity = new Vector2(Velocity.X, JUMP_VELOCITY);
+        }
+
+        // Horizontal movement (momentum-based)
+        float direction = Input.GetAxis("left", "right");
+
+        _breaking = Mathf.Sign(direction) != Mathf.Sign(Velocity.X) && direction != 0;
+
+        if (direction != 0)
+        {
+            if (_particle.ProcessMaterial is ParticleProcessMaterial mat)
+            {
+                mat.Direction = new Vector3(Mathf.Sign(-direction), -0.3f, 0);
+            }
+
+            float targetSpeed = direction * MAX_SPEED;
+            Velocity = new Vector2(
+                Mathf.MoveToward(
+                    Velocity.X,
+                    targetSpeed,
+                    ACCELERATION * dt * (1.0f + (_breaking ? 1.0f : 0.0f))
+                ),
+                Velocity.Y
+            );
+
+            _animationPlayer.FlipH = direction < 0;
+        }
+        else
+        {
+            Velocity = new Vector2(
+                Mathf.MoveToward(Velocity.X, 0.0f, FRICTION * dt),
+                Velocity.Y
+            );
+        }
+
+        UpdateAnimation();
+        MoveAndSlide();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (spraying)
+        {
+            if (_animationPlayer.Animation != "Paint" || !_animationPlayer.IsPlaying())
+            {
+                spraying = false;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        float speed = Mathf.Abs(Velocity.X);
+
+        if (!IsOnFloor())
+        {
+            _animationPlayer.Play("Jump");
+        }
+        else if (_breaking && speed > 5.0f)
+        {
+            _animationPlayer.Play("Breaking");
+            _particle.Emitting = true;
+        }
+        else if (speed < 5.0f)
+        {
+            _animationPlayer.Play("Idle");
+            _particle.Emitting = false;
+        }
+        else if (speed < WALK_SPEED)
+        {
+            _animationPlayer.Play("Run");
+            _particle.Emitting = false;
+        }
+        else
+        {
+            _animationPlayer.Play("Sprint");
+            _particle.Emitting = false;
+        }
+    }
+
+    private void SetupCameraLimits()
+    {
+        if (Tilemap == null)
+            return;
+
+        Rect2I usedRect = Tilemap.GetUsedRect();
+        Vector2I tileSize = Tilemap.TileSet.TileSize;
+
+        _camera.LimitLeft = usedRect.Position.X * tileSize.X;
+        _camera.LimitRight = (usedRect.Position.X + usedRect.Size.X) * tileSize.X;
+        _camera.LimitBottom = (usedRect.Position.Y + usedRect.Size.Y) * tileSize.Y;
+    }
+}
