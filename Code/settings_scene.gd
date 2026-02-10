ce2141ca12cdb9bc53a7aa38ded2f5360e86301a
Andrewm3568 <2300324@abertay.ac.uk>
2026-02-10 12:55:36 +0000

    added screen shake toggle to settings, added pause menu, linked up all buttons to switch scenes

--- Code/settings_scene.gd
+++ Code/settings_scene.gd
@@ -4,6 +4,10 @@ var masterVolume = AudioServer.get_bus_index("Master")
 var musicVolume = AudioServer.get_bus_index("Music")
 var sfxVolume = AudioServer.get_bus_index("SFX")
 
+var screenShake
+
+const LEVEL_SELECT := preload("res://Scenes/Level/studio.tscn")
+
 func _on_master_slider_value_changed(masterValue: float) -> void:
 	AudioServer.set_bus_volume_db(masterVolume, linear_to_db(masterValue))
 
@@ -12,3 +16,9 @@ func _on_music_slider_value_changed(musicValue: float) -> void:
 
 func _on_sound_effect_slider_value_changed(sfxValue: float) -> void:
 	AudioServer.set_bus_volume_db(sfxVolume, linear_to_db(sfxValue))
+
+func _on_screen_shake_button_toggled(toggled_on: bool) -> void:
+	screenShake = toggled_on;
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_packed(LEVEL_SELECT)
