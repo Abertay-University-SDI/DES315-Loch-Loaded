c586ee756be384cc48b731d7f589f4bae99f5608
PiotrMazanik <2202552@uad.ac.uk>
2026-02-12 21:31:00 +0000

    added full controller support for ui

--- Code/level.gd
+++ Code/level.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @onready var pause_menu = $CanvasLayer/PauseScene
+@onready var resume_button = $CanvasLayer/PauseScene/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/BackGameButton
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
+		resume_button.grab_focus()
 		pause_menu.show()
 		get_tree().paused = true
--- Code/settings_scene.gd
+++ Code/settings_scene.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@export var back_button:Button
+
 var masterVolume = AudioServer.get_bus_index("Master")
 var musicVolume = AudioServer.get_bus_index("Music")
 var sfxVolume = AudioServer.get_bus_index("SFX")
@@ -61,3 +63,8 @@ func _on_back_button_pressed() -> void:
 	save_settings_to_file()
 	hide();
 	get_tree().get_nodes_in_group("current_ui").back().show()
+
+
+func _on_back_button_visibility_changed() -> void:
+	if (back_button.visible):
+		back_button.grab_focus()
