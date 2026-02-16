2d9c498907a028f944550414ea5272d921588858
PiotrMazanik <2202552@uad.ac.uk>
2026-02-16 18:57:56 +0000

    added dynamic label for input

--- Code/spray_spot.gd
+++ Code/spray_spot.gd
@@ -4,9 +4,30 @@ extends Node2D
 var player_in_zone := false
 var spray_scene:PackedScene
 
+var last_input_was_controller :bool= false
+
+func get_spray_button_text() -> String:
+	var events = InputMap.action_get_events("spray")
+
+	for e in events:
+		if last_input_was_controller and e is InputEventJoypadButton:
+			return "Right Bumper"
+		elif not last_input_was_controller and e is InputEventKey:
+			return e.as_text().split(" ")[0]
+
+	return ""
+
+func _input(event):
+	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		last_input_was_controller = true
+	elif event is InputEventKey or event is InputEventMouseButton:
+		last_input_was_controller = false
+
+
 func _on_zone_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		player_in_zone = true
+		label.text = 'Press "%s" to paint' % get_spray_button_text()
 		label.show()
 
 func _on_zone_body_exited(body: Node) -> void:
