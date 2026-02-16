e174eeea4d36427523548297044b14a7a2b1bf42
PiotrMazanik <2202552@uad.ac.uk>
2026-02-16 19:44:04 +0000

    fixed a scene issue

--- Code/pause_scene.gd
+++ Code/pause_scene.gd
@@ -10,7 +10,7 @@ func _on_back_game_button_pressed() -> void:
 
 func _on_back_studio_button_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Scenes/Level/studio.tscn")
+	get_tree().change_scene_to_file("res://Scenes/UI/studio.tscn")
 
 
 func _on_settings_button_pressed() -> void:
