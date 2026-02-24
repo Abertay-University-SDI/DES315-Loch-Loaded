1cd2118a8685dd72cc3dafe6331e0bd68571a040
PiotrMazanik <2202552@uad.ac.uk>
2026-02-24 19:53:35 +0000

    added zippy functionality

--- Code/interactables/zipline.gd
+++ Code/interactables/zipline.gd
@@ -1,19 +1,28 @@
 @tool
 extends Node2D
 
-@export var point_0:Marker2D
-@export var point_1:Marker2D
-@export var line:Line2D
+@export var point_0: Marker2D
+@export var point_1: Marker2D
+@export var line: Line2D
+@export var collision_shape: CollisionShape2D
 
+var dir :Vector2
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _process(delta: float) -> void:
+	if !point_0 or !point_1:
+		return
+	
+	# Update line
 	line.global_position = point_0.global_position
 	line.clear_points()
 	line.add_point(Vector2.ZERO)
-	line.add_point(point_1.global_position-point_0.global_position)
+	line.add_point(point_1.global_position - point_0.global_position)
+	
+	collision_shape.global_position = Vector2.ZERO
+	collision_shape.shape.a = point_0.global_position
+	collision_shape.shape.b = point_1.global_position
+	
+	dir = -(point_0.global_position-point_1.global_position).normalized()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_dir()->Vector2:
+	return dir
