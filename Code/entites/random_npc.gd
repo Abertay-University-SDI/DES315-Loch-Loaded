59c8116dde39b9eb870087c689b764e5e0284be2
Andrewm3568 <2300324@abertay.ac.uk>
2026-03-27 02:08:44 +0000

    fixed npc sprite error

--- Code/entites/random_npc.gd
+++ Code/entites/random_npc.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sprite.frame = randi_range(0,sprite.hframes)
+	sprite.frame = randi_range(0,sprite.hframes - 1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
