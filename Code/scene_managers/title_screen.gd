50f1ad319b0599d98385c048aefc40e4fc9cbfe5
PiotrMazanik <2202552@uad.ac.uk>
2026-04-28 14:38:57 +0100

    added Button for tutorial

--- Code/scene_managers/title_screen.gd
+++ Code/scene_managers/title_screen.gd
@@ -37,3 +37,6 @@ func _on_credits_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_tutorial_button_pressed() -> void:SceneTransition.transition_to_path("res://Scenes/Level/Tutorial_Level.tscn")
